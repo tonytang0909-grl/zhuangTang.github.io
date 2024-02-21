ee7d5256cb832f08ca0225a87f5c0b44a126f315
Zhuang T <tonytang1199@gmail.com>
2024-02-21 11:54:52 +1030

    Fix GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? 'https://github.com/tonytang0909-grl/porto' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/porto/' : '/',
   transpileDependencies: true
 })
