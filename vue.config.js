af0e5c94101df78d1ae62a9227e0063121113452
Zhuang T <tonytang1199@gmail.com>
2024-02-21 13:07:29 +1030

    ...

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/porto/dist/' : '/',
+  publicPath: '/porto/public/',
   transpileDependencies: true
 })
